73406a89a361e0e36f55a6c55fbfbe74b8d57623
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-12-16 23:43:29 +0200

    Exercises Indices and Data Aggregation

--- 01. MS SQL/06. Indices and Data Aggregation/Part II – Queries for SoftUni Database.sql
+++ 01. MS SQL/06. Indices and Data Aggregation/Part II – Queries for SoftUni Database.sql
@@ -6,10 +6,29 @@ FROM Employees AS E
 GROUP BY DepartmentID
 
 -- 14. Employees Minimum Salaries
-
+SELECT 
+DepartmentID
+,MIN(SALARY) AS [MinimumSalary]
+FROM Employees
+WHERE DepartmentID IN (2, 5, 7) AND HireDate > '01/01/2000'
+GROUP BY DepartmentID
 
 -- 15. Employees Average Salaries
+SELECT * INTO EmployeesEM
+	FROM Employees
 
+DELETE FROM EmployeesEM
+	WHERE ManagerID = 42
+
+UPDATE EmployeesEM
+	SET Salary += 5000
+	WHERE DepartmentID = 1
+
+SELECT 
+DepartmentID
+,AVG(Salary) AS [AverageSalary]
+FROM EmployeesEM
+GROUP BY DepartmentID
 -- 16. Employees Maximum Salaries
 
 -- 17. Employees Count Salaries
