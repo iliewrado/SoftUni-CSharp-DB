799e5730a56df6405ed18044a853e1d30a19224a
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-12-16 23:57:33 +0200

    Exercises Indices and Data Aggregation

--- 01. MS SQL/06. Indices and Data Aggregation/Part II – Queries for SoftUni Database.sql
+++ 01. MS SQL/06. Indices and Data Aggregation/Part II – Queries for SoftUni Database.sql
@@ -14,26 +14,41 @@ WHERE DepartmentID IN (2, 5, 7) AND HireDate > '01/01/2000'
 GROUP BY DepartmentID
 
 -- 15. Employees Average Salaries
-SELECT * INTO EmployeesEM
-	FROM Employees
+SELECT *
+INTO TempTable
+FROM Employees
+WHERE Salary > 30000
 
-DELETE FROM EmployeesEM
-	WHERE ManagerID = 42
+DELETE FROM TempTable
+WHERE ManagerID = 42
 
-UPDATE EmployeesEM
-	SET Salary += 5000
-	WHERE DepartmentID = 1
+UPDATE TempTable
+SET Salary += 5000
+WHERE DepartmentID = 1
 
 SELECT 
 DepartmentID
-,AVG(Salary) AS [AverageSalary]
-FROM EmployeesEM
+,AVG(Salary) AS AverageSalary
+FROM TempTable
 GROUP BY DepartmentID
+
 -- 16. Employees Maximum Salaries
+SELECT 
+DepartmentID
+,MAX(Salary) AS [MaxSalary]
+FROM Employees
+GROUP BY DepartmentID
+HAVING NOT MAX(Salary) BETWEEN 30000 AND 70000
 
 -- 17. Employees Count Salaries
+SELECT 
+COUNT(Salary) AS [Count]
+FROM Employees
+GROUP BY ManagerID 
+HAVING ManagerID IS NULL
 
 -- 18. *3rd Highest Salary
+
 
 -- 19. **Salary Challenge
 
