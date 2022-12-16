f6625f3c11d4326f5649cb6771b498b3eca6a3b5
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-12-16 23:04:33 +0200

    Exercises Indices and Data Aggregation

---
+++ 01. MS SQL/06. Indices and Data Aggregation/Part II – Queries for SoftUni Database.sql
@@ -0,0 +1,20 @@
+-- 13. Departments Total Salaries
+SELECT 
+DepartmentID
+,SUM(Salary) AS [TotalSalary]
+FROM Employees AS E 
+GROUP BY DepartmentID
+
+-- 14. Employees Minimum Salaries
+
+
+-- 15. Employees Average Salaries
+
+-- 16. Employees Maximum Salaries
+
+-- 17. Employees Count Salaries
+
+-- 18. *3rd Highest Salary
+
+-- 19. **Salary Challenge
+
