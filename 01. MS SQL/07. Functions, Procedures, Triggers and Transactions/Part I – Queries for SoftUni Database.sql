a9246c33288c29719933293a1732dba6c9e6e60e
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-12-24 23:21:08 +0200

    Exercises Functions and Stored Procedures

--- 01. MS SQL/07. Functions, Procedures, Triggers and Transactions/Part I – Queries for SoftUni Database.sql
+++ 01. MS SQL/07. Functions, Procedures, Triggers and Transactions/Part I – Queries for SoftUni Database.sql
@@ -37,3 +37,56 @@ BEGIN
 END
 GO
 
+-- 4.	Employees from Town
+CREATE PROC usp_GetEmployeesFromTown (@townName NVARCHAR(50))
+AS
+BEGIN
+	SELECT 
+		FirstName
+		,LastName
+	FROM Employees AS E
+	 LEFT JOIN Addresses AS A ON E.AddressID = A.AddressID
+	 LEFT JOIN Towns AS T ON A.TownID = T.TownID
+	WHERE T.[Name] = @townName
+END
+GO
+
+-- 5.	Salary Level Function
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(10)
+AS 
+BEGIN 
+	DECLARE @salaryLevel VARCHAR(10)
+		IF @salary < 30000 
+			BEGIN
+				SET @salaryLevel = 'Low'
+			END
+		ELSE IF @salary > 50000 
+			BEGIN
+				SET @salaryLevel = 'High'
+			END
+		ELSE 
+			BEGIN 
+				SET @salaryLevel = 'Average'
+			END
+	RETURN @salaryLevel
+END
+GO
+
+-- 6.	Employees by Salary Level
+CREATE PROC usp_EmployeesBySalaryLevel (@salaryLevel VARCHAR(10))
+AS
+BEGIN
+	SELECT 
+		FirstName
+		,LastName
+	FROM Employees
+	WHERE @salaryLevel = [dbo].ufn_GetSalaryLevel(Salary)
+END
+GO
+
+-- 7.	Define Function
+
+
+-- 8.	* Delete Employees and Departments
+
