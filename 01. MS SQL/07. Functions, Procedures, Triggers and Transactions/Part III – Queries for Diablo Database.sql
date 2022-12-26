0e2219d82880947a4c7a2ba4ba67d565c42b8e4b
iliewrado <87384771+iliewrado@users.noreply.github.com>
2022-12-26 19:37:54 +0200

    Exercises Functions and Stored Procedures

--- 01. MS SQL/07. Functions, Procedures, Triggers and Transactions/Part III – Queries for Diablo Database.sql
+++ 01. MS SQL/07. Functions, Procedures, Triggers and Transactions/Part III – Queries for Diablo Database.sql
@@ -3,4 +3,17 @@ USE Diablo
 GO
 
 -- 13.	*Scalar Function: Cash in User Games Odd Rows
-
+CREATE FUNCTION ufn_CashInUsersGames (@gamename NVARCHAR(50))
+RETURNS TABLE
+AS
+	RETURN (SELECT 
+			SUM(Cash) AS [SumCash]
+			FROM (
+				SELECT
+				U.Cash 
+				,ROW_NUMBER() OVER(ORDER BY U.Cash DESC) AS Ordered
+				FROM Games AS G
+				JOIN UsersGames AS U ON G.Id = U.GameId
+				WHERE G.[Name] = @gamename) AS [Rows]
+			WHERE Ordered %2 != 0)
+GO
