2a7a584cc82c5eb29a69486aa22cb9164d095c2e
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-01-01 21:53:07 +0200

    Exercises Triggers and Transactions

--- 01. MS SQL/07. Functions, Procedures, Triggers and Transactions/Part II – Queries for Bank Database.sql
+++ 01. MS SQL/07. Functions, Procedures, Triggers and Transactions/Part II – Queries for Bank Database.sql
@@ -125,5 +125,18 @@ END
 GO
 
 -- 18.	Money Transfer
-
-
+CREATE PROC usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(15, 4))
+AS
+BEGIN 
+	BEGIN TRANSACTION
+        IF (@Amount <= 0)
+        BEGIN
+            RAISERROR('Amount cannot be negative or zero', 16, 1)
+            ROLLBACK
+            RETURN
+        END
+        EXEC usp_DepositMoney @ReceiverId, @Amount
+        EXEC usp_WithdrawMoney @SenderId, @Amount
+        COMMIT
+END
+GO
