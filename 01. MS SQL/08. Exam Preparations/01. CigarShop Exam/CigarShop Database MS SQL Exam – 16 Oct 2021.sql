2d6cd1ddd3d57aaa7e85cd2424d7e2cc3889e755
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-01-02 22:15:28 +0200

    Update CigarShop Database MS SQL Exam – 16 Oct 2021.sql

--- 01. MS SQL/08. Exam Preparations/01. CigarShop Exam/CigarShop Database MS SQL Exam – 16 Oct 2021.sql
+++ 01. MS SQL/08. Exam Preparations/01. CigarShop Exam/CigarShop Database MS SQL Exam – 16 Oct 2021.sql
@@ -61,3 +61,22 @@ ClientId INT NOT NULL FOREIGN KEY REFERENCES Clients(Id)
 PRIMARY KEY (ClientId, CigarId)
 )
 
+-- Section 2. DML (10 pts)
+-- Before you start you have to import "01-DDL-Data-Seeder.sql ".
+-- If you have created the structure correctly the data should be successfully inserted.
+
+-- 2.	Insert
+INSERT INTO Cigars
+(CigarName,	BrandId, TastId, SizeId, PriceForSingleCigar, ImageURL)
+VALUES
+('COHIBA ROBUSTO',	9,	1,	5,	15.50,	'cohiba-robusto-stick_18.jpg')
+,('COHIBA SIGLO I',	9,	1,	10,	410.00,	'cohiba-siglo-i-stick_12.jpg')
+,('HOYO DE MONTERREY LE HOYO DU MAIRE',	14,	5, 11, 7.50, 'hoyo-du-maire-stick_17.jpg')
+,('HOYO DE MONTERREY LE HOYO DE SAN JUAN',	14,	4, 15,	32.00, 'hoyo-de-san-juan-stick_20.jpg')
+,('TRINIDAD COLONIALES',	2,	3,	8,	85.21,	'trinidad-coloniales-stick_30.jpg')
+
+INSERT INTO Addresses (Town, Country, Streat, ZIP)
+VALUES
+('Sofia', 'Bulgaria', '18 Bul. Vasil levski', '1000')
+,('Athens', 'Greece', '4342 McDonald Avenue', '10435')
+,('Zagreb', 'Croatia', '4333 Lauren Drive', '10000')
