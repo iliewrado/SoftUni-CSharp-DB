16cef2e10d65777a7e1062e69ac8a23e63210726
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-01-03 23:15:16 +0200

    Exam Preparation

--- 01. MS SQL/08. Exam Preparations/02. ZOO Exam/ZOO Database Basics MS SQL Exam – 19 06 2022.sql
+++ 01. MS SQL/08. Exam Preparations/02. ZOO Exam/ZOO Database Basics MS SQL Exam – 19 06 2022.sql
@@ -14,3 +14,54 @@ USE Zoo
 GO
 
 -- 1.	Database design
+CREATE TABLE Owners(
+Id INT PRIMARY KEY IDENTITY
+,[Name] VARCHAR(50) NOT NULL
+,PhoneNumber VARCHAR(15) NOT NULL
+,[Address] VARCHAR(50)
+)
+
+CREATE TABLE AnimalTypes(
+Id INT PRIMARY KEY IDENTITY
+,AnimalType VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Cages(
+Id INT PRIMARY KEY IDENTITY
+,AnimalTypeId INT NOT NULL FOREIGN KEY REFERENCES AnimalTypes(Id)
+)
+
+CREATE TABLE Animals(
+Id INT PRIMARY KEY IDENTITY
+,[Name] VARCHAR(30) NOT NULL
+,BirthDate DATE NOT NULL
+,OwnerId INT FOREIGN KEY REFERENCES Owners(Id)
+,AnimalTypeId INT NOT NULL FOREIGN KEY REFERENCES AnimalTypes(Id)
+)
+
+CREATE TABLE AnimalsCages(
+CageId INT NOT NULL FOREIGN KEY REFERENCES Cages(Id)
+,AnimalId INT NOT NULL FOREIGN KEY REFERENCES Animals(Id)
+PRIMARY KEY (CageId ,AnimalId)
+)
+
+CREATE TABLE VolunteersDepartments(
+Id INT PRIMARY KEY IDENTITY
+, DepartmentName VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Volunteers(
+Id INT PRIMARY KEY IDENTITY
+,[Name] VARCHAR(50) NOT NULL
+,PhoneNumber VARCHAR(15) NOT NULL
+,[Address] VARCHAR(50) 
+,AnimalId INT FOREIGN KEY REFERENCES Animals(Id)
+,DepartmentId INT NOT NULL FOREIGN KEY REFERENCES VolunteersDepartments(Id)
+)
+
+
+-- Section 2. DML (10 pts)
+-- Before you start you have to import "01. DDL_Dataset.sql ". 
+-- If you have created the structure correctly the data should be successfully inserted. 
+
+-- 2.	Insert
