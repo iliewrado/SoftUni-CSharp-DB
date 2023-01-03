4d4902bfbcd403fe5e5e535328718e9dc69c8194
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-01-03 22:34:10 +0200

    Exam Preparation

---
+++ 01. MS SQL/08. Exam Preparations/02. ZOO Exam/ZOO Database Basics MS SQL Exam – 19 06 2022.sql
@@ -0,0 +1,16 @@
+-- Database Basics MS SQL Exam – 19 06 2022
+-- N.B.
+-- Use VARCHAR for strings, not NVARCHAR. 
+-- Keep in mind that Judge doesn’t accept the “ALTER” statement 
+-- and square brackets naming when the names are not keywords.
+
+-- Section 1. DDL (30 pts)
+CREATE DATABASE Zoo
+
+GO
+
+USE Zoo
+
+GO
+
+-- 1.	Database design
