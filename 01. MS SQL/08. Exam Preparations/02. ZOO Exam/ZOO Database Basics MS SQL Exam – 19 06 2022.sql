76feed2e7f13f646e63705ebe9745156e581b2e2
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-01-05 21:44:53 +0200

    Exam Preparation

--- 01. MS SQL/08. Exam Preparations/02. ZOO Exam/ZOO Database Basics MS SQL Exam – 19 06 2022.sql
+++ 01. MS SQL/08. Exam Preparations/02. ZOO Exam/ZOO Database Basics MS SQL Exam – 19 06 2022.sql
@@ -138,8 +138,21 @@ SELECT
 FROM Animals AS A
 JOIN Owners AS O ON A.OwnerId = O.Id
 JOIN AnimalsCages AS C ON A.Id = C.AnimalId
+JOIN AnimalTypes AS T ON A.AnimalTypeId = T.Id
+WHERE T.AnimalType = 'Mammals'
 ORDER BY O.[Name], A.[Name] DESC
 
 -- 9.	Volunteers in Sofia
+SELECT 
+V.[Name]
+,V.PhoneNumber
+,REPLACE(REPLACE(V.[Address],' Sofia ', 'Sofia'),'Sofia, ', '') AS [Address]
+FROM Volunteers AS V
+JOIN VolunteersDepartments AS D ON V.DepartmentId = D.Id
+WHERE D.DepartmentName = 'Education program assistant' 
+AND V.Address LIKE '%Sofia%'
+ORDER BY V.[Name]
+
+-- 10.	Animals for Adoption
 
 
