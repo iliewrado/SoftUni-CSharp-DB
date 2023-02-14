b77a8e450904f641880a6232b57ac41efc3a78ce
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-02-14 22:17:03 +0200

    AdvancedQuerying

--- 02. Entity Framework Core/06. Advanced Querying/BookShop.Data/Configuration.cs
+++ 02. Entity Framework Core/06. Advanced Querying/BookShop.Data/Configuration.cs
@@ -2,7 +2,10 @@
 {
     internal class Configuration
     {
-        internal static string ConnectionString 
-            => "Server=.;Database=BookShop;Integrated Security=True;";
+        //internal static string ConnectionString
+        //    => "Server=.;Database=BookShop;Integrated Security=True;";
+
+        internal static string ConnectionString
+            => "Server=.;Database=BookShop;User Id=sa;Password=Radoslav!;encrypt=false;";
     }
 }
--- 02. Entity Framework Core/06. Advanced Querying/BookShop/StartUp.cs
+++ 02. Entity Framework Core/06. Advanced Querying/BookShop/StartUp.cs
@@ -1,7 +1,13 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
+    using Castle.Core.Internal;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore.Migrations.Operations;
+    using System;
+    using System.Linq;
+    using System.Text;
 
     public class StartUp
     {
@@ -9,6 +15,47 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            //Console.WriteLine(GetBooksByAgeRestriction(db, ToTitle(Console.ReadLine())));
+            Console.WriteLine(GetGoldenBooks(db));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            string result = string.Empty;
+
+            bool isParsed = Enum.TryParse(command, true, out AgeRestriction ageRestriction);
+
+            if (isParsed)
+            {
+                var restrictedBooks = context.Books
+                .Where(b => b.AgeRestriction.Equals(ageRestriction))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+                result = String.Join(Environment.NewLine, restrictedBooks);
+            }
+
+            return result.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var goldenBooks = context.Books
+                .Where(b => b.Copies < 5000 && (int)b.EditionType == 2)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            return String.Join(Environment.NewLine, goldenBooks).TrimEnd();
+        }
+
+        public static string ToTitle(string text)
+        {
+            if (text.IsNullOrEmpty())
+                return null;
+            return System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(text);
         }
     }
 }
