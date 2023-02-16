7df228009b80f091d9ee47103471d0a300757fc3
iliewrado <87384771+iliewrado@users.noreply.github.com>
2023-02-16 21:23:50 +0200

    Advanced Querying

--- 02. Entity Framework Core/06. Advanced Querying/BookShop/StartUp.cs
+++ 02. Entity Framework Core/06. Advanced Querying/BookShop/StartUp.cs
@@ -262,6 +262,31 @@
             return result.ToString().TrimEnd();
         }
 
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Copies < 4200)
+                .ToList();
+
+            context.Remove(books);
+            context.SaveChanges();
+            return books.Count;
+        }
+
         public static string ToTitle(string text)
         {
             if (text.IsNullOrEmpty())
